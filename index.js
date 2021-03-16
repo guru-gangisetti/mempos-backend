839daed423941615bd660d8ba9bfb03d4b746441
Guru Gangisetti <gurugangisetti@IS-5241s-MacBook-Pro.local>
2021-03-16 00:35:02 -0700

    updated mongodb url

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.get( '/', (req, res)=>{
     res.send('Hellow Memories API');
 });
 
-//const CONNECTION_URL = "mongodb+srv://gurubrahmam:Sairam12@cluster0.zkm75.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://gurubrahmam:Sairam12@cluster0.zkm75.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000; 
 
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology : true})
+mongoose.connect(CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology : true})
             .then(() => app.listen(PORT, ()=> console.log(`Server running on : ${PORT} `) ))
             .catch(() => console.log(error));
 
